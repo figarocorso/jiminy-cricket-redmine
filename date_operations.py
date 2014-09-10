1f603d8a8df4a6283bd31808e10b7a97ee7ab132
Miguel Julián <mjulian@zentyal.com>
2014-09-10 09:38:13 +0200

    Added date comparison

--- date_operations.py
+++ date_operations.py
@@ -1,2 +1,10 @@
-def date_interval(date, interval):
-    return False
+from pytz import timezone
+from datetime import datetime
+
+# interval in minutes
+def date_interval(compared_date, interval):
+    now = datetime.now(timezone('UTC'))
+    compared_date = datetime.strptime(compared_date, "%Y-%m-%dT%H:%M:%SZ")
+    compared_date = compared_date.replace(tzinfo=timezone('UTC'))
+
+    return (now - compared_date).total_seconds() >= (int(interval) * 60)
